a9eebc5c41c4ebe84c7b0035121b0c4c3612c0c0
Cryptonean <faizanvit8@gmail.com>
2025-04-08 10:41:31 +0530

    emission validator param

---
+++ src/config/scripts/emission/scripts.ts
@@ -0,0 +1,98 @@
+import {
+  applyDoubleCborEncoding,
+  applyParamsToScript,
+  Data,
+  mintingPolicyToId,
+  Script,
+  Validator,
+} from "@lucid-evolution/lucid";
+
+import {
+  cet_minter_cet_minter_mint,
+  user_script_user_script_spend,
+} from "./plutus";
+
+import {
+  config_datum_holder_config_datum_holder_spend,
+  identification_nft_identification_nft_mint,
+  validator_contract_validator_contract_mint,
+  validator_contract_validator_contract_mint_mint,
+} from "../karbonLedger";
+import { identificationPolicyid } from "@/config";
+
+const cet_minter_mint = applyDoubleCborEncoding(cet_minter_cet_minter_mint);
+
+export const CETMINTER: Validator = {
+  type: "PlutusV3",
+  script: cet_minter_mint,
+};
+
+const user_script_spend = applyDoubleCborEncoding(
+  user_script_user_script_spend
+);
+
+export const USERSCRIPT: (param: any[]) => Validator = (param: any[]) => {
+  return {
+    type: "PlutusV3",
+    script: applyParamsToScript(user_script_spend, param),
+  };
+};
+
+// ----
+//------------------------------------------------------------------
+
+const identificationNFT_Mint = applyDoubleCborEncoding(
+  identification_nft_identification_nft_mint
+);
+
+export function IdentificationNFT_MintValidator(params: any[]): Validator {
+  return {
+    type: "PlutusV3",
+    script: applyParamsToScript(identificationNFT_Mint, params),
+  };
+}
+
+const configdatumholderscript = applyDoubleCborEncoding(
+  config_datum_holder_config_datum_holder_spend
+);
+
+export function ConfigDatumHolderValidator(): Validator {
+  return {
+    type: "PlutusV3",
+    script: configdatumholderscript,
+  };
+}
+
+const ValidatorContractScript = applyDoubleCborEncoding(
+  validator_contract_validator_contract_mint
+);
+export function COTMINTER(): Validator {
+  //config_nft : PolicyId; validator_contract_mint: PolicyId
+  const validatorMinterParam = getPolicyId(ValidatorMinter);
+  return {
+    type: "PlutusV3",
+    script: applyParamsToScript(ValidatorContractScript, [
+      identificationPolicyid, //identification pid from karbon-ledger
+      validatorMinterParam,
+    ]),
+  };
+}
+
+const ValidatorMinterScript = applyDoubleCborEncoding(
+  validator_contract_validator_contract_mint_mint
+);
+export function ValidatorMinter(): Validator {
+  //config_nft : PolicyId;
+  return {
+    type: "PlutusV3",
+    script: applyParamsToScript(ValidatorMinterScript, [
+      identificationPolicyid, //identification pid from karbon-ledger
+    ]),
+  };
+}
+
+export function getPolicyId(validatorFunction: { (): Validator; (): Script }) {
+  const validator: Validator = validatorFunction();
+  const policyID = mintingPolicyToId(validator);
+  return policyID;
+}
