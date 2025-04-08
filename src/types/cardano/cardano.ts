020c6cb101467172b9f47660d75c8cd998294939
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-04-08 09:49:05 +0530

    regitry tx

--- src/types/cardano/cardano.ts
+++ src/types/cardano/cardano.ts
@@ -1,7 +1,56 @@
-import { WalletApi } from "@lucid-evolution/lucid";
+import { Constr, Data, WalletApi } from "@lucid-evolution/lucid";
 
 export type CardanoWallet = {
   name: string;
   icon: string;
   enable(): Promise<WalletApi>;
 };
+
+//#region Utils Types
+
+export const OrefSchema = Data.Object({
+  transaction_id: Data.Bytes(),
+  output_index: Data.Integer(),
+});
+
+export const PaymentKeyHashSchema = Data.Bytes();
+export const StakeKeyHashSchema = Data.Bytes();
+
+export const AddressSchema = Data.Tuple([
+  PaymentKeyHashSchema,
+  StakeKeyHashSchema,
+]);
+
+export type Action = "Mint" | "Burn";
+export const Action = {
+  Mint: {
+    Title: "Mint",
+    Schema: Data.Literal("Mint"),
+    Constr: new Constr(0, []),
+  },
+  Burn: {
+    Title: "Burn",
+    Schema: Data.Literal("Burn"),
+    Constr: new Constr(1, []),
+  },
+};
+export const ActionSchema = Data.Enum([Action.Mint.Schema, Action.Burn.Schema]);
+
+export type AcceptRejectAction = "Accept" | "Reject";
+export const AcceptRejectAction = {
+  Accept: {
+    Title: "Accept",
+    Schema: Data.Literal("Accept"),
+    Constr: new Constr(0, []),
+  },
+  Reject: {
+    Title: "Reject",
+    Schema: Data.Literal("Reject"),
+    Constr: new Constr(1, []),
+  },
+};
+export const AcceptRejectActionSchema = Data.Enum([
+  AcceptRejectAction.Accept.Schema,
+  AcceptRejectAction.Reject.Schema,
+]);
+//#endregion
