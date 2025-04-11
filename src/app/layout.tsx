e78a4c6fe1bd85315b537c060edd4b4a972391b0
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-04-11 11:47:51 +0530

    ico and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 import "./global.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -6,9 +6,19 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Navbar from "@/components/Navbar";
 import CardanoProvider from "@/context/cardanoProvider";
+import { Metadata } from "next";
 
 const queryClient = new QueryClient();
-
+export const metadata: Metadata = {
+  title: {
+    default: "Karbon Compass",
+    template: `%s - ${"Karbon Compass"}`,
+  },
+  description: "siteConfig.description",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 export default function RootLayout({
   children,
 }: {
@@ -18,14 +28,14 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <CardanoProvider>
-          <QueryClientProvider client={queryClient}>
-            <TooltipProvider>
-              {/* <Toaster /> */}
-              <Sonner position="top-right" />
-              <Navbar />
-              {children}
-            </TooltipProvider>
-          </QueryClientProvider>
+          {/* <QueryClientProvider client={queryClient}> */}
+          <TooltipProvider>
+            {/* <Toaster /> */}
+            <Sonner position="top-right" />
+            <Navbar />
+            {children}
+          </TooltipProvider>
+          {/* </QueryClientProvider> */}
         </CardanoProvider>
       </body>
     </html>
